feac8cde0ad8e86e6a7c455236e34feb79e40af3
veha <uchihaveha@gmail.com>
2020-02-15 17:14:37 +0200

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,18 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'prettier', 'prettier/react'],
+  plugins: ['react-hooks'],
   rules: {
-    "prettier/prettier": ["error", {
-      "singleQuote": true,
-      "trailingComma": "none",
-      "bracketSpacing": true,
-      "jsxBracketSameLine": false
-    }]
-  },
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'none',
+        bracketSpacing: true,
+        jsxBracketSameLine: false
+      }
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  }
 };
---
+++ __tests__/App.test.js
@@ -0,0 +1,14 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+import App from '../App';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+// jest.mock('react-native-firebase');
+it('renders correctly', () => {
+  renderer.create(<App />);
+});
